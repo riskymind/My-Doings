3dd9ca3c113c9521f8eccc114e4e2b6c81972831
kelechi <oparaiskelechi@gmail.com>
2021-10-08 00:42:22 +0100

    attach fragment to nav graph

---
+++ app/src/main/java/com/asterisk/mydoings/ui/add_edit_todo/AddEditTodoFragment.kt
@@ -0,0 +1,7 @@
+package com.asterisk.mydoings.ui.add_edit_todo
+
+import androidx.fragment.app.Fragment
+import com.asterisk.mydoings.R
+
+class AddEditTodoFragment: Fragment(R.layout.fragment_add_edit_todo) {
+}
---
+++ app/src/main/java/com/asterisk/mydoings/ui/todo/TodoFragment.kt
@@ -0,0 +1,8 @@
+package com.asterisk.mydoings.ui.todo
+
+import androidx.fragment.app.Fragment
+import com.asterisk.mydoings.R
+
+class TodoFragment: Fragment(R.layout.fragment_todo) {
+
+}
